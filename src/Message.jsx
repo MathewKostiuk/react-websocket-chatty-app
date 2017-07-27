7dd289313a33dc4b4a135a876e22865156c44636
Mathew Kostiuk <mathewkostiuk@gmail.com>
2017-07-27 18:13:55 +0000

    finally got notifications to work with no bugs

--- src/Message.jsx
+++ src/Message.jsx
@@ -16,11 +16,13 @@ class Message extends Component {
 
   render() {
     return (
+      <div>
       <div className='message'>
         <span className="message-username">{ this.props.username }</span>
         <span className="message-content">{ this.props.content }</span>
-        {this.parseType()}
       </div>
+        {this.parseType()}
+        </div>
       );
   }
 }
